8f8d286215f7edd658526089fccc2d184a00c7f6
KonstantinTheHandsome <508691@student.fontys.nl>
2023-10-31 17:40:46 +0100

    ohhh

--- ASS_ver1.py
+++ ASS_ver1.py
@@ -1,7 +1,17 @@
 import pandas as pd
 import numpy as np
 
+# Read the Excel file into a DataFrame
 df = pd.read_excel('Line Production September 2023.xlsx')
+
+# Create a filtered DataFrame without the excluded columns
+filtered_df = df.drop(columns=["Product", "deadline", "penalty cost"])
+
+# Calculate the orderScore and add it to the filtered DataFrame
+filtered_df['orderScore'] = df['deadline'] / df['penalty cost']
+
+# Sort the DataFrame by "orderScore" in ascending order
+filtered_df = filtered_df.sort_values(by='orderScore', ascending=True)
 
 products = []
 for i in range(1, len(df) + 1):
@@ -10,13 +20,28 @@ for i in range(1, len(df) + 1):
 deadline = df["deadline"]
 penaltyCost = df["penalty cost"]
 
+print(filtered_df)
 
 
-# dictionary maybe
-# Create a filtered DataFrame without the excluded columns
-filtered_df = df.drop(columns=(["Product", "deadline", "penalty cost"]))
+
 # Convert the filtered DataFrame to a NumPy matrix
 processingTimes = filtered_df.to_numpy()
+chosenline= np.zeros((len(products),7))
+
+startTime = 0
+currentTime = 0
+
+for coordinate, value in np.ndenumerate(processingTimes):
+    print("a")
+
+
+Line1 = []
+Line2 = []
+Line3 = []
+Line4 = []
+Line5 = []
+Line6 = []
+Line7 = []
 
 
 
@@ -27,6 +52,24 @@ processingTimes = filtered_df.to_numpy()
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
 
 
 
